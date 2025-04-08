0cbac57f2cc893131b8354cb3cbdf846deb8f1f2
xh20220630 <haox84961@gmail.com>
2025-04-08 23:23:30 +0800

    feat:添加match

--- src/composite_type/my_match/mod.rs
+++ src/composite_type/my_match/mod.rs
@@ -77,5 +77,46 @@ pub fn main() {
 
     print!("{:?}", filter_coins_enum_arr);
 
+    let boolean = true;
 
+    // 使用 match 表达式填空，并满足以下条件
+    //
+    // boolean = true => binary = 1
+    // boolean = false => binary = 0
+    let binary = match boolean{
+        true => 1,
+        false => 0
+    };
+
+    assert_eq!(binary, 1);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (_, second, ..) => {
+            println!("Some numbers: {}", second)
+        },
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n)  =>{ 
+            if n == y{
+                println!("Matched, n = {}", n)
+            }
+        },
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {}", x, y);
+
+    match 1 {
+        num @ (1 | 2) => {
+            println!("{}", num);
+        }
+        _ => {}
+    }
 }
